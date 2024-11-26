b8f18c3ac9651eefb45e920ab06a6dfb3d177ef6
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-26 12:00:25 +0500

    Add files via upload

---
+++ concatenateSTRINGinARRAY.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+		
+			char inputting[100];
+			cout<<"Enter Argument: ";
+			cin.getline(inputting, 100);
+			cout<<"Something "<< inputting;
+		return 0;
+}
